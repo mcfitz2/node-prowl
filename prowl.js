1820438a825e444114064a6870456697276ef07b
Micah Fitzgerald <mcfitz2@gmail.com>
2014-06-29 21:04:32 -0400

    added error checking and cleaned up code

--- prowl.js
+++ prowl.js
@@ -1,68 +1,70 @@
 var request = require("request");
-var parseXML = require("xml2js").parseString;
+var parseString = require("xml2js").parseString;
 var Prowl = function(provider_key) {
     this.provider_key = provider_key;
     this.base_url = "https://api.prowlapp.com/publicapi/";
+    this.remaining = null;
+    this.resetTimestamp = null;
+};
+Prowl.prototype._parseResponse = function(xml, callback) {
+    var response = {};
+    parseString(xml, function(err, json) {	
+	if (err) {
+	    return callback(err, null);
+	}
+	if (json.prowl.error) {
+	    return callback(json.prowl.error[0].$, null);
+	}
+	response.success = json.prowl.success[0].$;
+	if (json.prowl.retreive) {
+	    response.retrieve = json.prowl.retrieve[0].$;
+	}
+	callback(null, response);
+    });
+};
+Prowl.prototype._createHandler = function(callback) {
+    var self = this;
+    return function(err, res, body) {
+	self._parseResponse(body, function(err, res) {
+	    self.remaining = res.success.remaining;
+	    self.resetTimestamp = res.success.resetdate;
+	    callback(err, res);
+	});
+    };
 };
 Prowl.prototype.add = function(params, callback) {
     var self = this;
-    if (!(params.apikey && params.application && params.event && params.description)) {
-	throw new Error("Missing parameters");
-    }
-    if (params.apikey instanceof Array) {
-	params.apikey = params.apikey.join(",");
-    }
+    if (!params.apikey) { throw new Error("Missing apiKey(s)"); }
+    if (!params.application) { throw new Error("Missing application name"); }
+    if (! (params.description || params.event)) { throw new Error("You must provide an event, a description or both"); }
+    if (params.apikey instanceof Array) { params.apikey = params.apikey.join(","); }
     params.providerkey = self.provider_key;
     request.post({
 	url:self.base_url+"add",
 	form:params
-    }, function(err, res, body) {
-	parseXML(body, function(err, json) {
-	    console.log(JSON.stringify(json.prowl, null, "\t"));
-	    if (json.prowl.error && !err) {
-		err = json.prowl.error;
-	    }
-	    callback(err, json.prowl);
-	});
-    }); 
+    }, self._createHandler(callback)); 
 };
 Prowl.prototype.token = function(callback) {
     var self =  this;
     if (!self.provider_key) {
 	throw new Error("This action requires a provider key");
     }
-    request.get({url:self.base_url+"retrieve/token", qs:{providerkey:self.provider_key}}, function(err, res, body) {
-	parseXML(body, function(err, json) {
-	    callback(err, json.prowl.retrieve[0].$);
-	});
-    });
+    request.get({url:self.base_url+"retrieve/token", qs:{providerkey:self.provider_key}}, self._createHandler(callback));
 };
 Prowl.prototype.apikey = function(token, callback) {
     var self = this;
     if (!self.provider_key) {
 	throw new Error("this action requires a provider key");
     }
-    request.get({url:self.base_url+"retrieve/apikey", qs:{providerkey:self.provider_key, token:token}}, function(err, res, body) {
-	parseXML(body, function(err, json) {
-	    callback(err, json.prowl.retrieve[0].$);
-	});
-    });
+    request.get({url:self.base_url+"retrieve/apikey", qs:{providerkey:self.provider_key, token:token}}, self._createHandler(callback));
 };
 Prowl.prototype.verify = function(params, callback) {
     var self = this;
     if (!params.apikey) {
-	throw new Error("missing parameters");
+	throw new Error("Missing apiKey");
     }
     params.providerkey = self.provider_key;
-    request.get({url:self.base_url+"verify", qs:params}, function(err, res, body) {
-	parseXML(body, function(err, json) {
-	    console.log(JSON.stringify(json.prowl, null, "\t"));
-	    if (json.prowl.error && !err) {
-		err = json.prowl.error;
-	    }
-	    callback(err, json.prowl);
-	});
-    });
+    request.get({url:self.base_url+"verify", qs:params}, self._createHandler(callback));
 };
 Prowl.prototype.middleware = function(callback) {
     var self = this;
